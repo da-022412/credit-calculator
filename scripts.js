3f104d3d94ed7f56538e9bd4e0b27c2cf7f82072
Dennis Acosta <email@dennisacosta.me>
2021-12-14 16:17:10 -0500

    Add scripts

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let total = document.getElementById('total').value;
+let eligible = document.getElementById('eligible');
+let cash = document.getElementById('cash-back');
+let boost = document.getElementById('boost-back');
+
+cash.innerHTML = total * 2;
